aa946b128a7b6ea7432dccaf84a524d2d509405c
김병준 <qudwns0524@gmail.com>
2018-05-12 10:12:23 -1100

    Update 1100.cpp

--- 1100.cpp
+++ 1100.cpp
@@ -4,14 +4,14 @@ int main() {
     int res = 0;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            std::cin >> c;
-            if (c == 'F') {
-                if (!((i + j) % 2)) {
+            std::cin >> c; //문자 입력
+            if (c == 'F') { //말이 위치할 경우
+                if (!((i + j) % 2)) { //칸의 색상이 하얀색일 경우
                     res++;
                 }
             }
         }
     }
-    std::cout << res;
+    std::cout << res; //출력
     return 0;
 }
