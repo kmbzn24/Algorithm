64c750dd9fda80ac3298036c79b0b847cb890510
김병준 <qudwns0524@gmail.com>
2018-05-18 09:30:35 -1100

    Update 9655.cpp

--- 9655.cpp
+++ 9655.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int n;
-    scanf("%d", &n);
-    printf((n % 2) ? "SK" : "CY");
+    scanf("%d", &n); //n값 입력
+    printf((n % 2) ? "SK" : "CY"); //홀수일 경우 "SK", 짝수일 경우 "CY" 출력
     return 0;
 }
